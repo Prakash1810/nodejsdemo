48a6c43e7ff395808671bdd30f344fc658d809ae
satz <satzapplications@gmail.com>
2018-10-10 09:47:50 +0530

    WIP : check mongodb connection and run user temp schema

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const mongoose = require('mongoose');
 const express = require('express');
+const UserTemp = require('./db/user-temp');
 
 const app = express();
 
@@ -9,3 +11,26 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('listening on port 3000!!')
 });
+
+mongoose.connect('mongodb://localhost:27017/core-api', { useNewUrlParser: true }).then( () => {
+    console.log("DB connection successful");
+},
+(err) => {
+    console.log("DB connection failed");
+});
+
+
+    new UserTemp({
+        email:"satz@mail.com",
+        password:'123456'
+    }).save()
+    .then(item => {
+        console.log("user temp added successfully");
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+
+
+
