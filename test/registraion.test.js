31be458c6c2e77a7b7e7d581aeaa4b8231326b8c
satz <satzapplications@gmail.com>
2019-02-25 16:35:51 +0530

    unit test case fixing

--- test/registraion.test.js
+++ test/registraion.test.js
@@ -148,7 +148,7 @@ describe('Registration module intergration test case:- /api/registraton', () => 
     });
 
     it('Successfully user registred', async (done) => {
-        await request(baseUrl)
+        request(baseUrl)
         .post('/api/registration')
         .set('Accept', 'application/json')
         .send({
@@ -166,7 +166,7 @@ describe('Registration module intergration test case:- /api/registraton', () => 
     });
 
     it('Validate email address already registred', async (done) => {
-        await request(baseUrl)
+        request(baseUrl)
         .post('/api/registration')
         .set('Accept', 'application/json')
         .send({
