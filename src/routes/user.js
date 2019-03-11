ab49a76a29a8a53d3a8ab9334daf5dee9979feef
satz <satzapplications@gmail.com>
2019-03-11 17:56:22 +0530

    Change exchange authorization api get to post

--- src/routes/user.js
+++ src/routes/user.js
@@ -86,7 +86,7 @@ router.patch('/change-password', auth, (req, res) => {
     }
 });
 
-router.get('/get-user-id', (req, res) => {
+router.post('/get-user-id', (req, res) => {
     try {
         if (req.headers.authorization) {
             user.getTokenToUserId(req, res);
