2b2cbc23400b7e3637aec301a720cdf009da580d
satz <satzapplications@gmail.com>
2019-03-19 13:38:46 +0530

    Try Catch declaration to the registration route file

--- src/routes/registration.js
+++ src/routes/registration.js
@@ -6,11 +6,16 @@ const controller = new Controller;
 
 let router = express.Router()
 router.post('/', (req, res) => {
-    let { error }  = registration.validate(req.body.data.attributes);
-    if (error) {
-        res.status(400).send(controller.errorFormat(error));
-    } else {
-        registration.post(req, res);
+    try {
+        let { error }  = registration.validate(req.body.data.attributes);
+        if (error) {
+            res.status(400).send(controller.errorFormat(error));
+        } else {
+            registration.post(req, res);
+        }
+    }
+    catch (err) {
+        return res.status(500).send(controller.errorMsgFormat({'message': err.message }));
     }
 });
 
