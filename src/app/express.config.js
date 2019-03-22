c60908206ce65d3cd5da9319f6fec949d0fce7a3
satz <satzapplications@gmail.com>
2019-03-22 13:36:15 +0530

    cors bug fixing

--- src/app/express.config.js
+++ src/app/express.config.js
@@ -9,12 +9,6 @@ const i18n       = require('i18n-nodejs');
 
 const app        = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
