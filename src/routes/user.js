c349f40c71aac9e83c4bdef6db482b88ab97fcce
Naveenkumar <naveenkumar.s@appleproitsolutions.com>
2019-03-26 12:12:03 +0530

    geetest backend api implement

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,7 @@ const password      = require('../core/password');
 const Controller    = require('../core/controller');
 const auth          = require("../middlewares/authentication");
 const registration  = require('../core/registration');
-
+const slide           = require('../core/geetest-captcha');
 const router        = express.Router();
 const controller    = new Controller;
 
@@ -161,6 +161,45 @@ router.patch('/disable', (req, res) => {
     catch (err) {
         return res.status(500).send(controller.errorMsgFormat({'message': err.message }, 'users', 500));
     }
+});
+
+router.get("/gt/register-slide", function (req, res) {
+
+    try {
+        // 向极验申请每次验证所需的challenge
+        slide.register(null, function (err, data) {
+            if (err) {
+                console.error(err);
+                res.status(500);
+                res.send(err);
+                return;
+            }
+
+            if (!data.success) {
+                // 进入 failback，如果一直进入此模式，请检查服务器到极验服务器是否可访问
+                // 可以通过修改 hosts 把极验服务器 api.geetest.com 指到不可访问的地址
+
+                // 为以防万一，你可以选择以下两种方式之一：
+
+                // 1. 继续使用极验提供的failback备用方案
+                //req.session.fallback = true;
+                res.send(data);
+
+                // 2. 使用自己提供的备用方案
+                // todo
+
+            } else {
+                // 正常模式
+                //req.session.fallback = false;
+                res.send(data);
+            }
+        });
+    }
+    catch (err) {
+        return res.status(500).send(controller.errorMsgFormat({'message': err.message }, 'users', 500));
+
+    }
+   
 });
 
 
