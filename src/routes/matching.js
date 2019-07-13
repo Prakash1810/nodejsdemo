eabacffcb149423fbb1ea42fe41e680291bf41c8
Apple-Mathan <madhankuamr.m@appleproitsolutions.com>
2019-07-13 15:28:48 +0530

    Add balance update in matching api

--- src/routes/matching.js
+++ src/routes/matching.js
@@ -58,6 +58,18 @@ router.post('/balance/query', auth, async (req, res) => {
     }
 })
 
+router.patch('/balance/update', auth, async (req, res) => {
+    try {
+        console.log('Data:',req.body.data.attributes)
+        req.body.data.attributes.user_id = Number(req.user.user_id);
+        console.log('Data:',req.body.data.attributes)
+        await matching.matchingEngineRequest('patch', 'balance/update', req.body, res);
+    } catch (err) {
+        return res.status(500).send(controller.errorMsgFormat({
+            'message': err.message
+        }, 'order-matching', 500));
+    }
+})
 
 //ORDER
 
