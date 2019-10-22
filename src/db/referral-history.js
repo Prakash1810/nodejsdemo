5c01acb349dd3cb774075b98ff8c737d4241de6b
mathanms <madhankumar.m@appleproitsolutions.com>
2019-10-22 23:41:15 +0530

    edited-kyc-update

--- src/db/referral-history.js
+++ src/db/referral-history.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const referralHistorySchema = mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
     referrer_code: { type: String, required: true },
+    amount:{type:String},
     created_date: { type: Date, default: Date.now }
 });
 
