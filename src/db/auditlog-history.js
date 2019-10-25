a2d0a1b23d7836dfe50222fd99b417a2f75524c2
mathanms <madhankumar.m@appleproitsolutions.com>
2019-10-25 12:58:45 +0530

    edited-active-referrer-code

--- src/db/auditlog-history.js
+++ src/db/auditlog-history.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const auditLogSchema = mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
-    request: String,
+    request: mongoose.Schema.Types.Mixed,
     response: String,
     path: String,
     ip_address: String,
