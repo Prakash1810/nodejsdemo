85262291b181a9c1d67a6171b2521b9779032bcc
mohammednowfil <mohammed.nowfil@appleproitsolutions.com>
2019-12-03 14:50:10 +0530

    check referal code is active

--- src/core/registration.js
+++ src/core/registration.js
@@ -49,17 +49,17 @@ class Registration extends Controller {
         // check email address already exits in user temp collections
         UserTemp.find({ email: req.body.data.attributes.email })
             .exec()
-            .then( async result => {
-                
+            .then(async result => {
+
                 let data = req.body.data.attributes
                 if (result.length) {
                     let salt = await bcrypt.genSalt(10);
-                    data.password = await bcrypt.hash(data.password,salt);
-                    let isChecked = await accountActive.findOne({ email: data.email,type_for:'register' });
+                    data.password = await bcrypt.hash(data.password, salt);
+                    let isChecked = await accountActive.findOne({ email: data.email, type_for: 'register' });
                     if (isChecked) {
-                        
+
                         if (isChecked.count < config.get('accountActiveRegister.hmt')) {
-                            await accountActive.findOneAndUpdate({ email: data.email ,type_for:'register' },
+                            await accountActive.findOneAndUpdate({ email: data.email, type_for: 'register' },
                                 {
                                     $inc: {
                                         count: 1
@@ -79,56 +79,59 @@ class Registration extends Controller {
                             }
                             else {
 
-                                await accountActive.findOneAndUpdate({ email: data.email ,type_for : 'register'},{count:0,create_date:moment().format('YYYY-MM-DD HH:mm:ss')});
-                                let user = await UserTemp.findOneAndUpdate({email:req.body.data.attributes.email},{
-                                    password:data.password
+                                await accountActive.findOneAndUpdate({ email: data.email, type_for: 'register' }, { count: 0, create_date: moment().format('YYYY-MM-DD HH:mm:ss') });
+                                let user = await UserTemp.findOneAndUpdate({ email: req.body.data.attributes.email }, {
+                                    password: data.password
                                 })
-                                await this.sendActivationEmail(user,'withoutResend');
+                                await this.sendActivationEmail(user, 'withoutResend');
                                 return res.status(200).json(this.successFormat({
                                     'message': `We have sent a verification email to your registered email address ${data.email}. Please follow the instructions in the email to verify your account.`,
-                                },user._id));
+                                }, user._id));
                             }
 
                         }
-                        else 
-                        {
+                        else {
                             if (isChecked.count > config.get('accountActiveRegister.limit')) {
-                                await accountActive.findOneAndUpdate({ email: data.email ,type_for:'register' },
-                                {
-                                    $inc: {
-                                        count: 1
-                                    },
-                                    create_date: moment().format('YYYY-MM-DD HH:mm:ss')
+                                await accountActive.findOneAndUpdate({ email: data.email, type_for: 'register' },
+                                    {
+                                        $inc: {
+                                            count: 1
+                                        },
+                                        create_date: moment().format('YYYY-MM-DD HH:mm:ss')
+                                    })
+                                let user = await UserTemp.findOneAndUpdate({ email: req.body.data.attributes.email }, {
+                                    $set: {
+                                        password: data.password
+                                    }
                                 })
-                                let user = await UserTemp.findOneAndUpdate({email:req.body.data.attributes.email},{$set:{
-                                    password:data.password
-                                }})
-                                await this.sendActivationEmail(user,'withoutResend');
+                                await this.sendActivationEmail(user, 'withoutResend');
                                 return res.status(202).send(this.successFormat({
-                                        "message": `We have sent a verification email to your registered email address ${data.email}. Please follow the instructions in the email to verify your account.`,
-                                        "warning": `Your are about to expend the maximum resend limit. ${config.get('accountActiveRegister.hmt') - isChecked.count + 1}  attempt${(config.get('accountActiveRegister.hmt') - isChecked.count) + 1 > 1 ? 's':''} left`,
-                                },user._id));
+                                    "message": `We have sent a verification email to your registered email address ${data.email}. Please follow the instructions in the email to verify your account.`,
+                                    "warning": `Your are about to expend the maximum resend limit. ${config.get('accountActiveRegister.hmt') - isChecked.count + 1}  attempt${(config.get('accountActiveRegister.hmt') - isChecked.count) + 1 > 1 ? 's' : ''} left`,
+                                }, user._id));
                             }
                         }
-                        
-                       
+
+
                     }
-                    let user = await UserTemp.findOneAndUpdate({email:req.body.data.attributes.email},{$set:{
-                        password:data.password
-                    }})
-                    await this.sendActivationEmail(user,'withoutResend');
+                    let user = await UserTemp.findOneAndUpdate({ email: req.body.data.attributes.email }, {
+                        $set: {
+                            password: data.password
+                        }
+                    })
+                    await this.sendActivationEmail(user, 'withoutResend');
                     return res.status(200).json(this.successFormat({
                         'message': `We have sent a verification email to your registered email address ${data.email}. Please follow the instructions in the email to verify your account.`,
-                    },user._id));
-                    
-                   
+                    }, user._id));
+
+
                 } else {
                     // check email address already exits in user temp collections
                     Users.find({ email: req.body.data.attributes.email })
                         .exec()
                         .then(result => {
                             if (result.length) {
-                                return res.status(400).send(this.errorFormat({ 'message': 'The email address you entered already exists.'}));
+                                return res.status(400).send(this.errorFormat({ 'message': 'The email address you entered already exists.' }));
                             }
 
                             return this.insertUser(req, res);
@@ -143,16 +146,21 @@ class Registration extends Controller {
         let data = req.body.data.attributes;
         if (data.referrer_code) {
             let check = await users.findOne({ referral_code: data.referrer_code });
-            if(!check.is_active){
-                return res.status(400).send(this.errorMsgFormat({
-                    'message': 'The referral code you entered is invalid. Please use a different referral code.'
-                }));
-            }
-            if (check.length==0) {
+            console.log(check)
+
+            if (check) {
+                if (!check.is_active) {
+                    return res.status(400).send(this.errorMsgFormat({
+                        'message': 'The referral code you entered is invalid. Please use a different referral code.'
+                    }));
+                }
+            } else {
                 return res.status(400).send(this.errorMsgFormat({
                     'message': 'The referral code you entered is invalid. Please enter a valid referral code.'
                 }));
+
             }
+
         }
 
         UserTemp.create({
@@ -161,16 +169,16 @@ class Registration extends Controller {
             referrer_code: data.referrer_code ? data.referrer_code : null
         }, async (err, user) => {
             if (err) {
-                
+
                 return res.status(500).json(this.errorFormat({ 'message': err.message }));
             } else {
                 // send activation email
                 let isChecked = await this.sendActivationEmail(user);
-               
+
                 if (isChecked) {
                     await new accountActive({
-                        email :data.email,
-                        type_for : 'register',
+                        email: data.email,
+                        type_for: 'register',
                     }).save();
                     return res.status(200).json(this.successFormat({
                         'message': `We have sent a verification email to your registered email address ${user.email}. Please follow the instructions in the email to verify your account.`,
@@ -196,23 +204,22 @@ class Registration extends Controller {
             "subject": "Confirm Your Registration",
             "email_for": "registration"
         };
-        console.log("hash:",encryptedHash);
+        console.log("hash:", encryptedHash);
         //check how to many time click a resend button
         await apiServices.sendEmailNotification(serviceData);
-        if(type == 'withoutResend')
-        {
+        if (type == 'withoutResend') {
             let checked = await mangHash.findOneAndUpdate({ email: user.email, is_active: false, type_for: "registration" })
-            if(checked){
-                checked.hash=encryptedHash;
+            if (checked) {
+                checked.hash = encryptedHash;
                 checked.count = 1
-                checked.created_date=moment().format('YYYY-MM-DD HH:mm:ss');
+                checked.created_date = moment().format('YYYY-MM-DD HH:mm:ss');
                 checked.save();
             }
-            else{
+            else {
                 await new mangHash({ email: user.email, hash: encryptedHash, type_for: "registration", created_date: moment().format('YYYY-MM-DD HH:mm:ss') }).save();
             }
         }
-        else{
+        else {
             let checkCount = await mangHash.findOne({ email: user.email, is_active: false, type_for: "registration" });
             if (checkCount) {
                 let count = checkCount.count;
@@ -222,7 +229,7 @@ class Registration extends Controller {
                 await new mangHash({ email: user.email, hash: encryptedHash, type_for: "registration", created_date: moment().format('YYYY-MM-DD HH:mm:ss') }).save();
             }
         }
-       
+
         return true;
     }
 
@@ -232,24 +239,23 @@ class Registration extends Controller {
             if (requestedData.type === 'registration') {
                 UserTemp.findById(req.body.data.id).exec()
                     .then(async (user) => {
-                        if(user)
-                        {
+                        if (user) {
                             let checkCount = await mangHash.findOne({ email: user.email, is_active: false, type_for: "registration" });
                             if (checkCount) {
-                               
+
                                 if (checkCount.count > config.get('site.hmtLink')) {
                                     await UserTemp.deleteOne({ email: user.email });
-                                    await accountActive.deleteOne({email:user.email,type_for:'register'})
+                                    await accountActive.deleteOne({ email: user.email, type_for: 'register' })
                                     await mangHash.findOneAndUpdate({ email: user.email, is_active: false, type_for: "registration" }, { is_active: true, created_date: moment().format('YYYY-MM-DD HH:mm:ss') })
                                     return res.status(400).send(this.errorMsgFormat({
                                         'message': `You've exceeded the maximum email resend limit. Please login and verify your email to continue.`
                                     }, 'users', 400));
                                 }
                                 else {
-                               
+
                                     // send activation email
                                     let check = this.sendActivationEmail(user);
-                                    if(check){
+                                    if (check) {
                                         return res.status(200).json(this.successFormat({
                                             'message': `We have resent a verification email to your registered email address (${user.email}).`,
                                         }, user._id));
@@ -257,18 +263,18 @@ class Registration extends Controller {
                                 }
                             }
                         }
-                        
-                        else  {
-                            return res.status(400).send(this.errorMsgFormat({ 'message': 'The email address cannot be found. Please register to continue.'}));
+
+                        else {
+                            return res.status(400).send(this.errorMsgFormat({ 'message': 'The email address cannot be found. Please register to continue.' }));
                         }
                     });
             } else if (requestedData.type === 'forget-password') {
-                return password.forgetPasswordResend(req,res)
+                return password.forgetPasswordResend(req, res)
             } else {
-                return res.status(400).send(this.errorMsgFormat({'message': 'Type not found.'}));
+                return res.status(400).send(this.errorMsgFormat({ 'message': 'Type not found.' }));
             }
         } else {
-            return res.status(400).send(this.errorMsgFormat({'message': 'User ID not found.'}));
+            return res.status(400).send(this.errorMsgFormat({ 'message': 'User ID not found.' }));
         }
     }
 
