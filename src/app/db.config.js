058c6c5012eacd2d878f29c127d7ddf1745d215e
mohammednowfil <mohammed.nowfil@appleproitsolutions.com>
2019-12-04 18:19:39 +0530

    mongo-cluster-added

--- src/app/db.config.js
+++ src/app/db.config.js
@@ -1,17 +1,17 @@
-const mongoose   = require('mongoose');
-const dotenv     = require('dotenv');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 // init environment configuration
 dotenv.config();
 // set mongoose Promise to Bluebird
 mongoose.Promise = Promise;
 // Exit application on error
 mongoose.connection.on('error', (err) => {
- console.log(`MongoDB connection error: ${err}`);
- process.exit(-1);
+  console.log(`MongoDB connection error: ${err}`);
+  process.exit(-1);
 });
 // print mongoose logs in debug env
 if (process.env.DEBUG === 'true') {
- mongoose.set('debug', true);
+  mongoose.set('debug', true);
 }
 /**
 * Connect to mongo db
@@ -22,10 +22,10 @@ if (process.env.DEBUG === 'true') {
 exports.connect = () => {
   //  mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_NAME}`,
   mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CONNECTION}`,
-  {
-   keepAlive: 1,
-   useNewUrlParser: true,
-   autoIndex: false
-});
- return mongoose.connection;
+    {
+      keepAlive: 1,
+      useNewUrlParser: true,
+      autoIndex: false
+    });
+  return mongoose.connection;
 };
