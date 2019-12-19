71350c38dd8f2f84ed6ba0cc227a8d21c45d1889
mohammednowfil <mohammed.nowfil@appleproitsolutions.com>
2019-12-19 21:24:13 +0530

    remove console

--- src/services/api.js
+++ src/services/api.js
@@ -71,11 +71,9 @@ class Api extends Controller {
     }
 
     axiosAPI(data) {
-        console.log("Dataaa:", data);
         axios.post(
             `${process.env.WALLETAPI}/api/${process.env.WALLETAPI_VERSION}/address/generate`, this.requestDataFormat(data)
         ).then(axiosResponse => {
-            console.log(axiosResponse.data)
             if (axiosResponse.data !== undefined) {
                 return axiosResponse.data;
             }
