779b73308bb011304d70e8e78738ce9b547660da
Lifwon <lifwon06@gmail.com>
2020-09-18 11:31:15 +0530

    maintenance endpoint adding.

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@ const walletRoutes = require('./wallet');
 const router = express.Router();
 const matchRoutes = require('./matching');
 const ieo = require('./ieo');
+const maintenance = require('./maintenance');
 
 router.use(express.static('dist'));
 router.use('/user/registration', registrationRoutes);
 router.use('/user', userRoutes);
 router.use('/wallet', walletRoutes);
 router.use('/matching', matchRoutes);
-router.use('/ieo',ieo)
+router.use('/ieo',ieo);
+router.use('/maintenance', maintenance);
 
 module.exports = router;
